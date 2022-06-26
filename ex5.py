f12a4511a71d00bfa4eeebf588c232faa6ca0ad5
Techkbenj <kbenjamin@campus.technion.ac.il>
2022-06-26 15:31:30 +0300

    adding func2

--- ex5.py
+++ ex5.py
@@ -1,8 +1,6 @@
 import json
 import os
 
-
-
 def names_of_registered_students(input_json_path, course_name):
     """
     This function returns a list of the names of the students who registered for
@@ -23,9 +21,21 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
-
-
+    new_dict = {}
+    with open(input_json_path, 'r') as input:
+        new_dict = json.load(input)
+    output_dict = {}
+    for id, student in new_dict.items():
+        for course in student["registered_courses"]:
+            output_dict[course] = 0
+    for course_name, students in output_dict:
+        students = len(names_of_registered_students(input_json_path), course_name)
+    with open(output_file_path, 'w') as output:
+        for course_name, students in output_dict:
+            output.write(course_name)
+            output.write(" ")
+            output.write(students)
+            output.write("\n")
 
 def courses_for_lecturers(json_directory_path, output_json_path):
     """
